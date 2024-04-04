76ae42373fe7d6b065f2ce7de024af04fa029e7d
Your Name <you@example.com>
2024-04-04 16:12:47 +0530

    add redirect amd form

--- src/main/java/com/example/demo/todoitem/TodoItem.java
+++ src/main/java/com/example/demo/todoitem/TodoItem.java
@@ -1,8 +1,41 @@
 package com.example.demo.todoitem;
 
 public class TodoItem {
-    public int itemId;
-    public String description;
-    public boolean isCompleted;
-    public String listId;
+    private int itemId;
+    private String description;
+    private boolean isCompleted;
+    private String listId;
+
+    public int getItemId() {
+        return itemId;
+    }
+
+
+    public boolean getIsCompleted() {
+        return isCompleted;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getListId() {
+        return listId;
+    }
+
+    public void setIsCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public void setListId(String listId) {
+        this.listId = listId;
+    }
 }
--- src/main/java/com/example/demo/todoitem/TodoItemUiController.java
+++ src/main/java/com/example/demo/todoitem/TodoItemUiController.java
@@ -2,20 +2,46 @@ package com.example.demo.todoitem;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("todoItem")
 public class TodoItemUiController {
-    @GetMapping
-    public String showTodoItem(Model model) {
-        TodoItem todoItem = new TodoItem();
-        todoItem.itemId = 12;
-        todoItem.description = "First Item";
-        todoItem.isCompleted = false;
-        model.addAttribute("todoItem", todoItem);
+    List<TodoItem> todos = new ArrayList<>();
+    @GetMapping("{id}")
+    public String showTodoItem(Model model, @PathVariable("id") int id) {
+        boolean found = false;
+        for (var item: todos) {
+            if (item.getItemId() == id) {
+                model.addAttribute("todoItem", item);
+                found = true;
+            }
+        }
+        if (!found) {
+            model.addAttribute("todoItem", new TodoItem());
+        }
         return "todoItem";
+    }
+
+    @GetMapping("add")
+    public String add(Model model) {
+        TodoItem todoItem = new TodoItem();
+        model.addAttribute("newTodoItem", todoItem);
+        return "newTodo";
+    }
+
+    private int currId = 1;
+    @PostMapping("addTodo")
+    public String addTodo(@ModelAttribute("newTodoItem") TodoItem newTodoItem) {
+        System.out.println(newTodoItem);
+        newTodoItem.setItemId(currId);
+        newTodoItem.setIsCompleted(false);
+        todos.add(newTodoItem);
+        currId++;
+        return "redirect:/todoItem/"+(currId-1);
     }
 
 
